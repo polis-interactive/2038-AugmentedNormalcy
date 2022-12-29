82ecfc4d4252245b2773a885dafacae1f9ba9a90
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2022-12-28 20:15:11 -0600

    Yeah, I blame windows. it took 93 milliseconds to run 100000 items through the queue on linux, I don't think it will be a bottleneck

--- prototype/test/test_utility/worker_thread.cpp
+++ prototype/test/test_utility/worker_thread.cpp
@@ -3,8 +3,6 @@
 //
 
 #include <doctest.h>
-#include <string>
-#include <utility>
 #include <vector>
 #include <iostream>
 #include <chrono>
@@ -16,12 +14,13 @@ typedef std::chrono::high_resolution_clock Clock;
 struct test_timestamp {
     test_timestamp() : inst(Clock::now()) {}
     std::chrono::time_point<Clock> inst;
+    std::chrono::time_point<Clock> out_stamp;
 };
 
 TEST_CASE("We start up and stop and delete without the compiler throwing a hissy fit") {
     {
         auto callback = [](std::shared_ptr<test_timestamp> &&ts) {
-            std::cout << Clock::now() - ts->inst << std::endl;
+            std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now() - ts->inst).count() << std::endl;
         };
         auto wt = utility::WorkerThread<test_timestamp>::CreateWorkerThread(callback);
         wt->Start();
@@ -31,6 +30,70 @@ TEST_CASE("We start up and stop and delete without the compiler throwing a hissy
     }
 }
 
+struct test_message {
+    explicit test_message(std::string &&s) : _msg(std::move(s)) {}
+    std::string _msg;
+};
+
 TEST_CASE("We can do some work!") {
-    std::vector<test_timestamp> output;
+    std::queue<std::shared_ptr<test_message>> output;
+    auto callback = [&output](std::shared_ptr<test_message> &&tm) {
+        output.push(tm);
+    };
+    auto wt = utility::WorkerThread<test_message>::CreateWorkerThread(callback);
+    wt->Start();
+
+    // add one, read it out
+    wt->PostWork(std::make_shared<test_message>("yolo"));
+    std::this_thread::sleep_for(0.1ms);
+    REQUIRE_EQ(output.size(), 1);
+    auto tm_1 = output.front();
+    output.pop();
+    REQUIRE_EQ(tm_1->_msg, "yolo");
+
+    wt->PostWork(std::make_shared<test_message>("message 1"));
+    wt->PostWork(std::make_shared<test_message>("message 2"));
+    wt->PostWork(std::make_shared<test_message>("message 3"));
+    std::this_thread::sleep_for(0.1ms);
+    tm_1 = output.front();
+    output.pop();
+    REQUIRE_EQ(tm_1->_msg, "message 1");
+    auto tm_2 = output.front();
+    output.pop();
+    REQUIRE_EQ(tm_2->_msg, "message 2");
+    auto tm_3 = output.front();
+    output.pop();
+    REQUIRE_EQ(tm_3->_msg, "message 3");
+
+    for (int i = 0; i < 1000; i++) {
+        wt->PostWork(std::make_shared<test_message>(std::to_string(i)));
+    }
+    std::this_thread::sleep_for(0.1ms);
+    REQUIRE_EQ(output.size(), 1000);
+    REQUIRE_EQ(output.front()->_msg, "0");
+    REQUIRE_EQ(output.back()->_msg, "999");
+
+    wt->Stop();
+}
+
+TEST_CASE("baby stress test") {
+    std::queue<std::shared_ptr<test_timestamp>> output;
+    auto callback = [&output](std::shared_ptr<test_timestamp> &&ts) {
+        ts->out_stamp = Clock::now();
+        output.push(ts);
+    };
+    auto wt = utility::WorkerThread<test_timestamp>::CreateWorkerThread(callback);
+    wt->Start();
+
+    for (int i = 0; i < 100000; i++) {
+        wt->PostWork(std::make_shared<test_timestamp>());
+    }
+    std::this_thread::sleep_for(500ms);
+    REQUIRE_EQ(output.size(), 100000);
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(
+            output.back()->out_stamp - output.front()->inst
+    ).count() << std::endl;
+
+    wt->Stop();
 }
