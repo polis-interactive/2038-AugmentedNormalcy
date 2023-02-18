cae18e8f40267edcd27870827554e3046a2de2b7
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-02-18 14:42:26 -0600

    That looks 'righter'

--- prototype/test/test_infrastructure/test_codec/codec.cpp
+++ prototype/test/test_infrastructure/test_codec/codec.cpp
@@ -241,14 +241,23 @@ private:
     }
     void createBuffer() {
 
+        const int width = 1536;
+        const int height = 864;
+        const int stride = 1536;
+
         v4l2_format format {};
         int ret;
-        format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUV420;
-        format.fmt.pix.width = 1536;
-        format.fmt.pix.height = 864;
-        format.fmt.pix.bytesperline = 1536;
-        format.fmt.pix.field = V4L2_FIELD_NONE;
+        format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
+        format.fmt.pix_mp.width = width;
+        format.fmt.pix_mp.height = height;
+        // We assume YUV420 here, but it would be nice if we could do something
+        // like info.pixel_format.toV4L2Fourcc();
+        format.fmt.pix_mp.pixelformat = V4L2_PIX_FMT_YUV420;
+        format.fmt.pix_mp.plane_fmt[0].bytesperline = stride;
+        format.fmt.pix_mp.field = V4L2_FIELD_ANY;
+        // this may need to be rec601...
+        format.fmt.pix_mp.colorspace = V4L2_COLORSPACE_REC709;
+        format.fmt.pix_mp.num_planes = 1;
         ret = xioctl(VIDIOC_S_FMT, &format);
         if (ret < 0) {
             std::cout << "couldn't SET FORMAT" << std::endl;
