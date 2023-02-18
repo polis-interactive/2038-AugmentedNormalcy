03f125a3a2110ab68e23a92251e397ba498038c3
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-02-18 17:18:35 -0600

    Maybe it was the profile?

--- prototype/internal/infrastructure/codec/v4l2/v4l2_encoder.cpp
+++ prototype/internal/infrastructure/codec/v4l2/v4l2_encoder.cpp
@@ -81,7 +81,7 @@ namespace Codec {
 
         // set profile
         ctrl.id = V4L2_CID_MPEG_VIDEO_H264_PROFILE;
-        ctrl.value = V4L2_MPEG_VIDEO_H264_PROFILE_CONSTRAINED_BASELINE;
+        ctrl.value = V4L2_MPEG_VIDEO_H264_PROFILE_BASELINE;
         if (xioctl(_encoder_fd, VIDIOC_S_CTRL, &ctrl) < 0)
             throw std::runtime_error("failed to set profile");
 
@@ -104,12 +104,11 @@ namespace Codec {
             throw std::runtime_error("failed to set bitrate");
 
         // set infinite gop
-        /*
+
         ctrl.id = V4L2_CID_MPEG_VIDEO_H264_I_PERIOD;
         ctrl.value = 2147483647;
         if (xioctl(_encoder_fd, VIDIOC_S_CTRL, &ctrl) < 0)
             throw std::runtime_error("failed to set bitrate");
-        */
 
         v4l2_format fmt = {};
         fmt.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
