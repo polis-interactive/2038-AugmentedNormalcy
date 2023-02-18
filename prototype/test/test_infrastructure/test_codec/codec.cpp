f0b9a312182f1d7f15ec8bba7478247085839645
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-02-18 15:29:33 -0600

    Loosing hope

--- prototype/test/test_infrastructure/test_codec/codec.cpp
+++ prototype/test/test_infrastructure/test_codec/codec.cpp
@@ -315,7 +315,7 @@ private:
         std::cout << "Main fd:" << _fd << std::endl;
         std::cout << "Export fd:" << expbuf.fd << std::endl;
         auto size = buffer.length;
-        _buffer = std::shared_ptr<CameraBuffer>(new CameraBuffer(mem, expbuf.fd, size), [](CameraBuffer *c) {});
+        _buffer = std::shared_ptr<CameraBuffer>(new CameraBuffer(mem, _fd, size), [](CameraBuffer *c) {});
 
         std::cout << "Created buffer" << std::endl;
     }
