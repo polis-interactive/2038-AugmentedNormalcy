1c0c13212020cdd89581fcec9141a245ac0e85f1
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-02-18 18:23:42 -0600

    Dma?

--- prototype/internal/infrastructure/codec/v4l2/v4l2_encoder.cpp
+++ prototype/internal/infrastructure/codec/v4l2/v4l2_encoder.cpp
@@ -152,7 +152,7 @@ namespace Codec {
         v4l2_requestbuffers reqbufs = {};
         reqbufs.count = camera_buffer_count;
         reqbufs.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
-        reqbufs.memory = V4L2_MEMORY_MMAP;
+        reqbufs.memory = V4L2_MEMORY_DMABUF;
         if (xioctl(_encoder_fd, VIDIOC_REQBUFS, &reqbufs) < 0) {
             throw std::runtime_error("request for output buffers failed");
         }
@@ -223,7 +223,7 @@ namespace Codec {
         buf.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
         buf.index = index;
         buf.field = V4L2_FIELD_NONE;
-        buf.memory = V4L2_MEMORY_MMAP;
+        buf.memory = V4L2_MEMORY_DMABUF;
         buf.length = 1;
         buf.m.planes = planes;
         buf.m.planes[0].m.fd = input_buffer->GetFd();
