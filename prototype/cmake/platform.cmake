2ee20b128cf3358a9b4cb88983a284da1a1e5cb5
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-02-19 20:52:18 -0600

    PTHREAD

--- prototype/cmake/platform.cmake
+++ prototype/cmake/platform.cmake
@@ -3,14 +3,15 @@ if (WIN32)
     # assume broose windows; if we need, we can further disambiguate here
     set(AN_PLATFORM BROOSE_WINDOWS_LAPTOP)
     set(AN_PLATFORM_NUM 1)
-elseif (NOT __arm__)
-    # assume broose linux; if we need, we can further disambiguate here
-    set(AN_PLATFORM BROOSE_LINUX_LAPTOP)
-    set(AN_PLATFORM_NUM 10)
 elseif(EXISTS opt/vc/include/bcm_host.h)
     # RPI; if we need, we can further disambiguate here (for instance, CM4 vs PI4B
     set(AN_PLATFORM RPI)
     set(AN_PLATFORM_NUM 20)
+# why is this true for the pi? changed order to weed it out for now
+elseif (NOT __arm__)
+    # assume broose linux; if we need, we can further disambiguate here
+    set(AN_PLATFORM BROOSE_LINUX_LAPTOP)
+    set(AN_PLATFORM_NUM 10)
 else()
     # jetson orion; if we need, we can further disambiguate here (for instance, CM4 vs PI4B
     set(AN_PLATFORM JETSON)
