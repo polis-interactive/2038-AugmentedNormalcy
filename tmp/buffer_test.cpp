09538d5cba57f568cfdd7c293e8e4be95449b8b6
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-03-30 18:05:29 -0500

    Trying with dmabuf before trying to mmap it

--- tmp/buffer_test.cpp
+++ tmp/buffer_test.cpp
@@ -8,16 +8,17 @@
 
 #include "NvUtils.h"
 #include "NvJpegEncoder.h"
+#include "NvBufSurface.h"
 #include "libjpeg-8b/jpeglib.h"
 
-int main(int argc, char *argv[]) {
+void user_buffer() {
     std::filesystem::path this_dir = TMP_DIR;
 
     auto in_frame = this_dir;
     in_frame /= "in.yuv";
 
     auto out_frame = this_dir;
-    out_frame /= "out.jpeg";
+    out_frame /= "out_user.jpeg";
 
     NvBuffer buffer(V4L2_PIX_FMT_YUV420M, 1536, 864, 0);
     buffer.allocateMemory();
@@ -37,4 +38,51 @@ int main(int argc, char *argv[]) {
 
     delete[] out_buf;
     delete jpegenc;
+}
+
+void dma_buffer() {
+
+    std::filesystem::path this_dir = TMP_DIR;
+
+    auto in_frame = this_dir;
+    in_frame /= "in.yuv";
+
+    auto out_frame = this_dir;
+    out_frame /= "out_dma.jpeg";
+
+    NvBufSurf::NvCommonAllocateParams params;
+    /* Create PitchLinear output buffer for transform. */
+    params.memType = NVBUF_MEM_SURFACE_ARRAY;
+    params.width = 1536;
+    params.height = 864;
+    params.layout = NVBUF_LAYOUT_PITCH;
+    params.colorFormat = NVBUF_COLOR_FORMAT_YUV420;
+
+    params.memtag = NvBufSurfaceTag_CAMERA;
+    int src_dma_fd = -1;
+    auto ret = NvBufSurf::NvAllocate(&params, 1, &src_dma_fd);
+
+    std::ifstream test_file_in(in_frame, std::ios::out | std::ios::binary);
+    read_dmabuf(src_dma_fd, 0, &test_file_in);
+    read_dmabuf(src_dma_fd, 1, &test_file_in);
+    read_dmabuf(src_dma_fd, 2, &test_file_in);
+
+    auto jpegenc = NvJPEGEncoder::createJPEGEncoder("jpenenc");
+    unsigned long out_buf_size = 1536 * 864 * 3 / 2;
+    auto *out_buf = new unsigned char[out_buf_size];
+
+    ret = jpegenc->encodeFromFd(src_dma_fd, JCS_YCbCr, &out_buf, out_buf_size, 75);
+
+    std::ofstream test_file_out(out_frame, std::ios::out | std::ios::binary);
+
+    test_file_out.write((char *) out_buf, out_buf_size);
+
+    delete[] out_buf;
+    delete jpegenc;
+
+}
+
+int main(int argc, char *argv[]) {
+    user_buffer();
+    dma_buffer();
 }
