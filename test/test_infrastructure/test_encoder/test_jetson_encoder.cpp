d869a03d3c5afcd02f7cba10a696e114d9e96907
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-04 21:22:41 -0500

    hmmmm

--- test/test_infrastructure/test_encoder/test_jetson_encoder.cpp
+++ test/test_infrastructure/test_encoder/test_jetson_encoder.cpp
@@ -47,6 +47,9 @@ TEST_CASE("INFRASTRUCTURE_ENCODER_JETSON_ENCODER-Encode_a_frame") {
     TestJetsonEncoderConfig conf;
 
     std::filesystem::path this_dir = TEST_DIR;
+    this_dir /= "test_infrastructure";
+    this_dir /= "test_encoder";
+
     auto in_frame = this_dir;
     in_frame /= "in.yuv";
 
