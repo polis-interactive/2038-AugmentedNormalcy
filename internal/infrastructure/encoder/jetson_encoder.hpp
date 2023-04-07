db059f0a3cdbbaaa68e2438924f77c1b210ec192
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-07 14:12:47 -0500

    This is confusing aff

--- internal/infrastructure/encoder/jetson_encoder.hpp
+++ internal/infrastructure/encoder/jetson_encoder.hpp
@@ -46,13 +46,9 @@ namespace infrastructure {
             return fd;
         }
         void SyncCpu() {
-            NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 0);
-            NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 1);
             NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 2);
         }
         void SyncGpu() {
-            NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 0);
-            NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 1);
             NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 2);
         }
         ~JetsonBuffer();
