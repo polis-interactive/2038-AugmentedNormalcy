b02d845fa85e01fb73ea270448ec521ca02f1be4
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-07 14:27:42 -0500

    only one should fail; goal is to get it to not

--- internal/infrastructure/encoder/jetson_encoder.hpp
+++ internal/infrastructure/encoder/jetson_encoder.hpp
@@ -46,17 +46,17 @@ namespace infrastructure {
             return fd;
         }
         void SyncCpu() {
-            NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 0);
+            // NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 0);
             NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 1);
             NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 2);
 
 
-            NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 0);
+            // NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 0);
             NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 1);
             NvBufSurfaceSyncForCpu(_nvbuf_surf, 0, 2);
 
 
-            NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 0);
+            // NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 0);
             NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 1);
             NvBufSurfaceSyncForDevice(_nvbuf_surf, 0, 2);
         }
