deaffab814605411bc4a1f7d05a5589da7192039
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-10 21:07:38 -0500

    AHHHHH

--- scratch/pi_decode/main.cpp
+++ scratch/pi_decode/main.cpp
@@ -144,7 +144,7 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "V4L2 Decoder got " << reqbufs.count << " capture buffers" << std::endl;
 
-    v4l2_plane planes[VIDEO_MAX_PLANES];
+    v4l2_plane planes[1];
     v4l2_buffer buffer = {};
     buffer.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
     buffer.memory = V4L2_MEMORY_MMAP;
@@ -179,13 +179,20 @@ int main(int argc, char *argv[]) {
     if (xioctl(encoder_fd, VIDIOC_QUERYBUF, &buffer) < 0)
         throw std::runtime_error("failed to query capture buffer");
 
-    auto capture_size = buffer.m.planes[0].length;
     auto capture_mem = mmap(
             nullptr, buffer.m.planes[0].length, PROT_READ | PROT_WRITE, MAP_SHARED, encoder_fd,
             buffer.m.planes[0].m.mem_offset
     );
     if (capture_mem == MAP_FAILED)
         throw std::runtime_error("failed to mmap capture buffer");
+
+
+    memcpy((void *)output_mem, (void *) in_buf.data(), input_size);
+
+    std::cout << "V4l2 Decoder queued output buffer" << std::endl;
+
+    if (xioctl(encoder_fd, VIDIOC_QBUF, &buffer) < 0)
+        throw std::runtime_error("failed to queue output buffer");
 
     // should have three planes but meh
     std::cout << "V4l2 Decoder MMAPed capture buffer with size like so: " <<
@@ -195,25 +202,6 @@ int main(int argc, char *argv[]) {
         throw std::runtime_error("failed to queue capture buffer");
 
     std::cout << "V4l2 Decoder queued capture buffer" << std::endl;
-
-
-    memcpy((void *)output_mem, (void *) in_buf.data(), input_size);
-
-    buffer = {};
-    memset(planes, 0, sizeof(planes));
-    buffer.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
-    buffer.index = 0;
-    buffer.field = V4L2_FIELD_NONE;
-    buffer.memory = V4L2_MEMORY_DMABUF;
-    buffer.length = 1;
-    buffer.m.planes = planes;
-    buffer.m.planes[0].m.fd = encoder_fd;
-    buffer.m.planes[0].bytesused = input_size;
-    buffer.m.planes[0].length = output_size;
-    if (xioctl(encoder_fd, VIDIOC_QBUF, &buffer) < 0)
-        throw std::runtime_error("failed to queue output buffer");
-
-    std::cout << "V4l2 Decoder Queued output buffer" << std::endl;
 
     /*
 
