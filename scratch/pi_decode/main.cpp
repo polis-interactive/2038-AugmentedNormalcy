94dab603c9a71064989f33d7ed767cdcc695140c
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-10 19:56:55 -0500

    I LOVE CHATGPT (METHINKS)

--- scratch/pi_decode/main.cpp
+++ scratch/pi_decode/main.cpp
@@ -20,6 +20,8 @@ int xioctl(int fd, unsigned long ctl, void *arg) {
 
 int main(int argc, char *argv[]) {
     const char device_name[] = "/dev/video11";
+    const int width = 1536;
+    const int height = 864;
     auto encoder_fd = open(device_name, O_RDWR, 0);
     if (encoder_fd == -1) {
         switch(errno) {
@@ -34,7 +36,29 @@ int main(int argc, char *argv[]) {
         };
         return 1;
     }
-    std::cout << "V4l2 Decoder opend fd: " << encoder_fd << std::endl;
+    std::cout << "V4l2 Decoder opened fd: " << encoder_fd << std::endl;
+
+    struct v4l2_format fmt = {0};
+    fmt.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
+    fmt.fmt.pix_mp.width = width;
+    fmt.fmt.pix_mp.height = height;
+    fmt.fmt.pix_mp.pixelformat = V4L2_PIX_FMT_JPEG;
+
+    if (xioctl(encoder_fd, VIDIOC_S_FMT, &fmt))
+        throw std::runtime_error("failed to set output caps");
+
+
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
+    fmt.fmt.pix_mp.width = width;
+    fmt.fmt.pix_mp.height = height;
+    fmt.fmt.pix_mp.pixelformat = V4L2_PIX_FMT_YUV420;
+
+    if (xioctl(encoder_fd, VIDIOC_S_FMT, &fmt))
+        throw std::runtime_error("failed to set output caps");
+
+    std::cout << "V4l2 Decoder setup caps" << std::endl;
+
+    /*
 
     v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
     if (xioctl(encoder_fd, VIDIOC_STREAMON, &type) < 0)
@@ -56,6 +80,8 @@ int main(int argc, char *argv[]) {
         throw std::runtime_error("failed to start capture streaming");
 
     std::cout << "V4l2 Decoder stopped" << std::endl;
+
+     */
 
     close(encoder_fd);
     std::cout << "V4l2 Decoder Closed" << std::endl;
