71ab5305fd3ec06a4eb9bd15a7190b32140ab3d0
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 18:34:26 -0500

    Find the hang

--- internal/infrastructure/decoder/v4l2_decoder.cpp
+++ internal/infrastructure/decoder/v4l2_decoder.cpp
@@ -401,7 +401,6 @@ namespace infrastructure {
              * export to dmabuf
              */
 
-            /*
             memset(&expbuf, 0, sizeof(expbuf));
             expbuf.type = buffer.type;
             expbuf.index = buffer.index;
@@ -409,7 +408,6 @@ namespace infrastructure {
 
             if (xioctl(_decoder_fd, VIDIOC_EXPBUF, &expbuf) < 0)
                 throw std::runtime_error("failed to export the capture buffer");
-            */
 
             /*
              * queue buffer
@@ -422,7 +420,7 @@ namespace infrastructure {
              * setup proxy
              */
 
-            auto downstream_buffer = new DecoderBuffer(buffer.index, -1, capture_mem, capture_size);
+            auto downstream_buffer = new DecoderBuffer(buffer.index, expbuf.fd, capture_mem, capture_size);
             _downstream_buffers.insert({ buffer.index, downstream_buffer });
         }
     }
