5af39ed71f47c3fae2d53f13318c41a4c4756bf7
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 22:07:20 -0500

    The memory regions are overlapping, or wrong?

--- test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
+++ test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
@@ -151,6 +151,8 @@ TEST_CASE("INFRASTRUCTURE_DECODER_V4L2_DECODER-Stress_test") {
         memcpy((void *)buffer->GetMemory(), (void *) in_buf.data(), input_size);
         buffer->SetSize(input_size);
         buffer = decoder->GetResizableBuffer();
+        memcpy((void *)buffer->GetMemory(), (void *) in_buf.data(), input_size);
+        buffer->SetSize(input_size);
         in_time = Clock::now();
         decoder->PostResizableBuffer(std::move(buffer));
 
