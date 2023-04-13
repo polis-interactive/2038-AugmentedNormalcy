38899c24424a9402a1fb6d14b65a6acd5b8528d9
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 20:36:08 -0500

    What if I change the order

--- scratch/pi_decode/main.cpp
+++ scratch/pi_decode/main.cpp
@@ -373,23 +373,6 @@ int main(int argc, char *argv[]) {
     test_file_out.flush();
     test_file_out.close();
 
-    /*
-     * do it more times
-     */
-    buffer = {};
-    memset(planes, 0, sizeof(planes));
-    buffer.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
-    buffer.index = 0;
-    buffer.field = V4L2_FIELD_NONE;
-    buffer.memory = V4L2_MEMORY_MMAP;
-    buffer.length = 1;
-    buffer.m.planes = planes;
-    buffer.m.planes[0].length = std::get<0>(output_params[0]);
-    buffer.m.planes[0].bytesused = input_size;
-    buffer.m.planes[0].m.mem_offset = std::get<1>(output_params[0]);
-    if (xioctl(decoder_fd, VIDIOC_DQBUF, &buffer) < 0)
-        throw std::runtime_error("failed to dequeue output buffer");
-
 
     memcpy(std::get<2>(output_params[1]), in_buf.data(), input_size);
 
