bb86e5ed8bac15f712a8786b64a7c1b3bf59530e
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 22:04:44 -0500

    But this should work

--- test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
+++ test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
@@ -149,8 +149,8 @@ TEST_CASE("INFRASTRUCTURE_DECODER_V4L2_DECODER-Stress_test") {
         decoder->Start();
 
         auto buffer = decoder->GetResizableBuffer();
-        // memcpy((void *)buffer->GetMemory(), (void *) in_buf.data(), input_size);
-        // buffer->SetSize(input_size);
+        memcpy((void *)buffer->GetMemory(), (void *) in_buf.data(), input_size);
+        buffer->SetSize(input_size);
         decoder->PostResizableBuffer(std::move(buffer));
 
         std::this_thread::sleep_for(100ms);
