5a714e7ad8e4afbe7fdd05ffc1a7d7318ce9cc52
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 21:47:57 -0500

    I don't get it man

--- test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
+++ test/test_infrastructure/test_decoder/test_v4l2_decoder.cpp
@@ -150,8 +150,6 @@ TEST_CASE("INFRASTRUCTURE_DECODER_V4L2_DECODER-Stress_test") {
 
         auto buffer = decoder->GetResizableBuffer();
         std::cout << buffer->GetMemory() << std::endl;
-        memcpy(buffer->GetMemory(), in_buf.data(), input_size);
-        buffer->SetSize(input_size);
         in_time = Clock::now();
         // decoder->PostResizableBuffer(std::move(buffer));
         std::this_thread::sleep_for(30ms);
