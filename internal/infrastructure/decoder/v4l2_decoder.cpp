14af94d3b020f41a9318bd6e7e9024d6c6259e51
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 23:23:06 -0500

    Where is my bug

--- internal/infrastructure/decoder/v4l2_decoder.cpp
+++ internal/infrastructure/decoder/v4l2_decoder.cpp
@@ -63,7 +63,7 @@ namespace infrastructure {
         std::array<char, 1990656> in_buf = {};
         test_in_file.read(in_buf.data(), input_size);
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 300; i++) {
             V4l2ResizableBuffer *buffer;
             {
                 std::lock_guard<std::mutex> lock(_available_upstream_buffers_mutex);
