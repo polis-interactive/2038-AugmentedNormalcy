44f6761d5e4d41914cfa5e085d4856bd7803000a
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-12 23:25:21 -0500

    found my error?

--- internal/infrastructure/decoder/v4l2_decoder.cpp
+++ internal/infrastructure/decoder/v4l2_decoder.cpp
@@ -68,27 +68,7 @@ namespace infrastructure {
 
             memcpy((void *)buffer->GetMemory(), (void *) in_buf.data(), input_size);
 
-            auto v4l2_rz_buffer = std::static_pointer_cast<V4l2ResizableBuffer>(buffer);
-
-            v4l2_plane planes[VIDEO_MAX_PLANES];
-            v4l2_buffer buf = {};
-            buf.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
-            buf.index = v4l2_rz_buffer->GetIndex();
-            buf.memory = V4L2_MEMORY_MMAP;
-            buf.length = 1;
-            buf.m.planes = planes;
-            buf.m.planes[0].bytesused = input_size;
-            if (xioctl(_decoder_fd, VIDIOC_QBUF, &buf) < 0)
-                throw std::runtime_error("failed to queue output buffer");
-
-            if (i == 0) {
-                if (xioctl(_decoder_fd, VIDIOC_DQBUF, &buf) < 0)
-                    throw std::runtime_error("failed to queue output buffer");
-
-
-                if (xioctl(_decoder_fd, VIDIOC_QBUF, &buf) < 0)
-                    throw std::runtime_error("failed to queue output buffer");
-            }
+            PostResizableBuffer(std::move(buffer));
             std::this_thread::sleep_for(30ms);
         }
 
