cdce16775ab2d2b6ce7552785a955e51439280ae
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-04-14 19:04:13 -0500

    Humor me

--- internal/infrastructure/decoder/v4l2_decoder.cpp
+++ internal/infrastructure/decoder/v4l2_decoder.cpp
@@ -197,6 +197,7 @@ namespace infrastructure {
             throw std::runtime_error("failed to queue output buffer");
 
 
+        /*
         if (!_is_primed) {
             if (xioctl(_decoder_fd, VIDIOC_DQBUF, &buf) < 0)
                 throw std::runtime_error("failed to dequeue output buffer primer");
@@ -206,7 +207,7 @@ namespace infrastructure {
 
             _is_primed = true;
         }
-
+*/
     }
 
     void V4l2Decoder::handleDownstream() {
