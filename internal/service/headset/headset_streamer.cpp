1499c8232b820f34207770047fd7d526f45578f1
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-05-28 03:49:05 -0500

    Just for kicks

--- internal/service/headset/headset_streamer.cpp
+++ internal/service/headset/headset_streamer.cpp
@@ -9,13 +9,13 @@
 static void turnOnScreen() {
     std::string command = "uhubctl -l 1-1 -a 1";
     std::cout << "HeadsetStreamer wants to turn on screen" << std::endl;
-    // std::system(command.c_str());
+    std::system(command.c_str());
 }
 
 static void turnOffScreen() {
     std::string command = "uhubctl -l 1-1 -a 0";
     std::cout << "HeadsetStreamer wants to turn OFF screen" << std::endl;
-    // std::system(command.c_str());
+    std::system(command.c_str());
 }
 
 namespace service {
