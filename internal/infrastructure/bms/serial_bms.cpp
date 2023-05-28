ac30fb50f1b949b6a8c5fb79dbbdb00350e1752f
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-05-27 23:56:11 -0500

    Let's try this

--- internal/infrastructure/bms/serial_bms.cpp
+++ internal/infrastructure/bms/serial_bms.cpp
@@ -61,10 +61,16 @@ namespace infrastructure {
 
     bool SerialBms::setupConnection() {
         std::cout << "SerialBms::setupConnection Opening file" << std::endl;
-        _port_fd = open("/dev/ttyAMA0", O_RDWR);
+        _port_fd = open("/dev/ttyAMA0", O_RDWR | O_NONBLOCK | O_NOCTTY);
         if (_port_fd < 0) {
             std::cout << "SerialBms::setupConnection failed to open /dev/ttyAMA0" << std::endl;
             return false;
+        }
+
+        int s = fcntl(_port_fd, F_GETFL);
+        if (s >= 0) s = fcntl(_port_fd, F_SETFL, s | O_NONBLOCK);
+        if (s < 0) {
+            std::cout << "SerialBms::setupConnection failed to set _port_fd to nonblocking" << std::endl;
         }
 
         struct termios tty;
@@ -74,28 +80,9 @@ namespace infrastructure {
             return false;
         }
 
-        tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-        tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-        tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
-        tty.c_cflag |= CS8; // 8 bits per byte (most common)
-        tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-        tty.c_lflag &= ~ICANON;
-        tty.c_lflag &= ~ECHO; // Disable echo
-        tty.c_lflag &= ~ECHOE; // Disable erasure
-        tty.c_lflag &= ~ECHONL; // Disable new-line echo
-        tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-        tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-
-        tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-        tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-        // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-        // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-        tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-        tty.c_cc[VMIN] = 0;
+        cfmakeraw(&tty);
+        tty.c_iflag |= IGNPAR;
+        tty.c_cflag |= CREAD | CLOCAL;
 
 
         std::cout << "SerialBms::setupConnection setting speed" << std::endl;
