5bd70b6f811df341bfb79397dfc3596376b31234
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2023-06-02 18:21:48 -0500

    tyring bigger timeout

--- internal/infrastructure/bms/serial_bms.cpp
+++ internal/infrastructure/bms/serial_bms.cpp
@@ -45,7 +45,7 @@ namespace infrastructure {
                     "/dev/ttyAMA0", BaudRate::B_9600, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE,
                     HardwareFlowControl::OFF, SoftwareFlowControl::OFF
                 );
-                serial_port->SetTimeout(_bms_read_timeout);
+                serial_port->SetTimeout(1000);
                 serial_port->Open();
                 readAndReport(serial_port);
             } catch (const std::exception& ex) {
